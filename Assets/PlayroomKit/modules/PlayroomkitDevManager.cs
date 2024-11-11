4f4a533d7056d4963740733babe185071d0062a4
momintlh <77355191+momintlh@users.noreply.github.com>
2024-11-11 16:43:44 +0500

    chore: prefabs overrides

--- Assets/PlayroomKit/modules/PlayroomkitDevManager.cs
+++ Assets/PlayroomKit/modules/PlayroomkitDevManager.cs
@@ -1,31 +1,30 @@
 using System;
 using System.Collections.Generic;
-using UBB;
 using UnityEngine;
 
 #if UNITY_EDITOR
 using ParrelSync;
+using UBB;
 #endif
 
 namespace Playroom
 {
     public class PlayroomkitDevManager : MonoBehaviour
     {
-        [SerializeField] private PlayroomKit.MockModeSelector mockMode = PlayroomKit.CurrentMockMode;
+        [SerializeField]
+        private PlayroomKit.MockModeSelector mockMode = PlayroomKit.CurrentMockMode;
 
 
-        [Tooltip("InsertCoin() must be called in order to connect PlayroomKit server.\n\nChoose the gameObject (with the script) which calls InsertCoin.\n\nRead More in the docs")]
+        [Tooltip(
+            "InsertCoin() must be called in order to connect PlayroomKit server.\n\nChoose the gameObject (with the script) which calls InsertCoin.\n\nRead More in the docs")]
         [SerializeField]
         private GameObject insertCoinCaller;
-        
+
         private static PlayroomkitDevManager Instance { get; set; }
 
 #if UNITY_EDITOR
         private void Awake()
         {
-#if UNITY_EDITOR
-            if (ClonesManager.IsClone()) UnityBrowserBridge.Instance.httpServerPort += 1;
-#endif
             if (Instance == null)
             {
                 Instance = this;
@@ -35,7 +34,7 @@ namespace Playroom
             {
                 Destroy(gameObject);
             }
-            
+
             UpdateMockMode();
         }
 
@@ -47,22 +46,18 @@ namespace Playroom
         private void UpdateMockMode()
         {
             PlayroomKit.CurrentMockMode = mockMode;
-            PlayroomKit.RegisterGameObject("InsertCoin", insertCoinCaller);
-            PlayroomKit.RegisterGameObject("devManager", gameObject);
+            UnityBrowserBridge.Instance.RegisterGameObject("InsertCoin", insertCoinCaller);
+            UnityBrowserBridge.Instance.RegisterGameObject("devManager", gameObject);
         }
 
-        // Called from JS side for onPlayerJoin
+        /// <summary>
+        /// This is invoked from the JS bridge, when OnPlayerJoin is called, this is used to pass the id from playroom to unity.
+        /// </summary>
+        /// <param name="playerId"></param>
         private void GetPlayerID(string playerId)
         {
             PlayroomKit.MockOnPlayerJoinWrapper(playerId);
         }
-
-        private string InvokeWaitForState(string stateValue)
-        {
-            Debug.Log(stateValue);
-            return stateValue;
-        }
-
 #endif
     }
 }
