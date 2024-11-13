d8c3ca21ce45b5058d735a8221b24a2013a9977e
momintlh <77355191+momintlh@users.noreply.github.com>
2024-11-13 17:24:24 +0500

    feat: player service

---
+++ Assets/PlayroomKit/modules/MockMode/BrowserMode/BrowserMockPlayerService.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+namespace Playroom
+{
+    public class BrowserMockPlayerService : PlayroomKit.Player.IPlayerBase
+    {
+        
+        #region State 
+        public void SetState(string key, int value, bool reliable = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(string key, float value, bool reliable = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(string key, bool value, bool reliable = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(string key, string value, bool reliable = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(string key, object value, bool reliable = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetState<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        public PlayroomKit.Player.Profile GetProfile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Action OnQuit(Action<string> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Kick(Action onKickCallBack = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WaitForState(string stateKey, Action<string> onStateSetCallback = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
