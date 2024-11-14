1610236b97cc1c0a2175405a93b4cbd810ee8b30
momintlh <77355191+momintlh@users.noreply.github.com>
2024-11-14 17:16:01 +0500

    fix: rpc browser

--- Assets/PlayroomKit/modules/MockMode/BrowserMode/PlayroomBrowserMockService.cs
+++ Assets/PlayroomKit/modules/MockMode/BrowserMode/PlayroomBrowserMockService.cs
@@ -186,32 +186,22 @@ namespace Playroom
 
     public class RPCBrowser : PlayroomKit.IRPC
     {
-        private UnityBrowserBridge _ubb;
-
-        public RPCBrowser()
-        {
-        }
-
         public void RpcRegister(string name, Action<string, string> rpcRegisterCallback, string onResponseReturn = null)
         {
-            if (_ubb)
-                _ubb = UnityBrowserBridge.Instance;
-
             string callbackKey = $"RpcEvent_{name}";
             GameObject callbackObject = new GameObject(callbackKey);
+            Debug.Log(callbackKey);
 
             MockCallbackInvoker invoker = callbackObject.AddComponent<MockCallbackInvoker>();
             invoker.SetCallback(rpcRegisterCallback, callbackKey);
-
-
             CallBacksHandlerMock.Instance.RegisterCallbackObject(callbackKey, callbackObject, "ExecuteCallback");
-            _ubb.CallJs("RpcRegister", callbackKey, null, false, name, onResponseReturn);
-            
+
+            UnityBrowserBridge.Instance.CallJs("RpcRegister", null, null, false, name,  callbackKey,onResponseReturn);
         }
 
         public void RpcCall(string name, object data, PlayroomKit.RpcMode mode, Action callbackOnResponse = null)
         {
-            _ubb.CallJs("RpcCall", null, null, false, name, data.ToString(), mode.ToString());
+            UnityBrowserBridge.Instance.CallJs("RpcCall", null, null, false, name, data.ToString(), mode.ToString());
         }
 
         public void RpcCall(string name, object data, Action callbackOnResponse = null)
