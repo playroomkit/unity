c5229ec437c1898575559dbaefb722a193f0b381
momintlh <77355191+momintlh@users.noreply.github.com>
2025-02-04 11:28:41 +0500

    chore: testing scene for turnbased

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using Playroom;
 using UnityEngine;
 
@@ -5,51 +6,33 @@ public class GameManager : MonoBehaviour
 {
     private PlayroomKit _kit;
 
-    private void Start()
+    private void Awake()
     {
         _kit = new();
+    }
 
+    private void Start()
+    {
         _kit.InsertCoin(new InitOptions()
         {
-            gameId = "[my game id]",
-            maxPlayersPerRoom = 8,
-            discord = true
-        }, () =>
-        {
-            // same event multiple RPCs
-            _kit.RpcRegister("A", A);
-            _kit.RpcRegister("A", A2);
+            turnBased = true,
+            maxPlayersPerRoom = 2,
+        }, OnLaunchCallBack);
+    }
 
-            // RPC Call From Another RPC
-            _kit.RpcRegister("host", (data, sender) =>
-            {
-                    Debug.Log("Host RPC CALLED");
-                    _kit.RpcCall("client", 1, PlayroomKit.RpcMode.ALL);
-            }
-            );
-            _kit.RpcRegister("client", (data, sender) =>
-            {
-                Debug.Log("client rpc called");
-            });
-        });
+    private void OnLaunchCallBack()
+    {
+        _kit.OnPlayerJoin(CreatePlayer);
+        
+        // _kit.get
+    }
+
+    private void CreatePlayer(PlayroomKit.Player player)
+    {
+        Debug.Log($"{player.id} joined the room!");
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            _kit.RpcCall("A", 1, PlayroomKit.RpcMode.HOST);
-
-        if (Input.GetMouseButtonDown(1))
-            _kit.RpcCall("host", 1, PlayroomKit.RpcMode.HOST);
-    }
-
-    private void A(string data, string sender)
-    {
-        Debug.Log($"[Unity] A called");
-    }
-
-    private void A2(string data, string sender)
-    {
-        Debug.LogWarning("[Unity] A2 data: " + data.ToString());
     }
 }
