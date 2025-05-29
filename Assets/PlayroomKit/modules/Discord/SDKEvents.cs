40648bec13b370338cdff6109383f8f27d2e3502
momintlh <77355191+momintlh@users.noreply.github.com>
2025-05-29 20:28:03 +0500

    feat: events enum

---
+++ Assets/PlayroomKit/modules/Discord/SDKEvents.cs
@@ -0,0 +1,12 @@
+namespace Discord
+{
+    public enum SDKEvent
+    {
+        READY,
+        ORIENTATION_UPDATE,
+        ENTITLEMENT_CREATE,
+        ACTIVITY_INSTANCE_PARTICIPANTS_UPDATE,
+        ACTIVITY_LAYOUT_MODE_UPDATE,
+        THERMAL_STATE_UPDATE,
+    }
+}
