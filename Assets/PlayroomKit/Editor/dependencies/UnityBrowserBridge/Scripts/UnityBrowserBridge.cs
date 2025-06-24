f918ff11da1263633f0f44274189379e901031a4
EffanByte <94035974+EffanByte@users.noreply.github.com>
2025-06-24 14:32:06 +0500

    fix: Unity Browser Bridge OpenQA requirement
    Browser bridge was using openQA, so made it run conditionally if using editor

--- Assets/PlayroomKit/Editor/dependencies/UnityBrowserBridge/Scripts/UnityBrowserBridge.cs
+++ Assets/PlayroomKit/Editor/dependencies/UnityBrowserBridge/Scripts/UnityBrowserBridge.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
+
+#if UNITY_EDITOR
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Opera;
 using OpenQA.Selenium.Safari;
+#endif
 
 namespace UBB
 {
