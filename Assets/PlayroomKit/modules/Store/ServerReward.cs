d05bfd54f14009c7b0f4b1781bb5dbdcdaa8e389
momintlh <77355191+momintlh@users.noreply.github.com>
2025-07-01 14:23:46 +0500

    feat: server reward class

---
+++ Assets/PlayroomKit/modules/Store/ServerReward.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Playroom
+{
+    public class ServerReward
+    {
+        public string serverId;
+        public string id;
+        public string title;
+        public string description;
+        public string image;
+        public string message;
+        public bool active;
+        public string key;
+        public string skuId;
+        public string type;
+        public bool status;
+    }
+}
