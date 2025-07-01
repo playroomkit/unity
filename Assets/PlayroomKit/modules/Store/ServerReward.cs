83b810b9c454012520c998b01a968842737d3e9d
momintlh <77355191+momintlh@users.noreply.github.com>
2025-07-01 15:08:23 +0500

    feat: parse to json

--- Assets/PlayroomKit/modules/Store/ServerReward.cs
+++ Assets/PlayroomKit/modules/Store/ServerReward.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
+using SimpleJSON;
 using UnityEngine;
 
 namespace Playroom
 {
+    [Serializable]
     public class ServerReward
     {
         public string serverId;
@@ -15,5 +19,42 @@ namespace Playroom
         public string skuId;
         public string type;
         public bool status;
+
+        private static ServerReward FromJSONNode(JSONNode node)
+        {
+            ServerReward reward = new ServerReward
+            {
+                serverId = node["server_id"]?.Value ?? string.Empty,
+                id = node["id"]?.Value ?? string.Empty,
+                title = node["title"]?.Value ?? string.Empty,
+                description = node["description"]?.Value ?? string.Empty,
+                image = node["image"]?.Value ?? string.Empty,
+                message = node["message"]?.Value ?? string.Empty,
+                active = node["active"] != null && node["active"].AsBool,
+                key = node["key"]?.Value ?? string.Empty,
+                skuId = node["sku_id"]?.Value ?? string.Empty,
+                type = node["type"]?.Value ?? string.Empty,
+                status = node["status"] != null && node["status"].AsBool
+            };
+
+            return reward;
+        }
+
+        public static List<ServerReward> FromJSON(string jsonString)
+        {
+            List<ServerReward> serverRewards = new();
+            JSONNode root = JSON.Parse(jsonString);
+
+            if (!root.IsArray)
+                Debug.LogWarning("Expected an array");
+
+            foreach (JSONNode item in root.AsArray)
+            {
+                var data = FromJSONNode(item);
+                serverRewards.Add(data);
+            }
+
+            return serverRewards;
+        }
     }
 }
